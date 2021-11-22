afa9c5245e38e0cf8bb03b17ea298d18481f2792
『TØNIC』 乂 ₭ILLΣR <57848595+Tonic990@users.noreply.github.com>
2021-11-22 15:54:11 +0700

    Update administrator.py

--- Music/MusicUtilities/helpers/administrator.py
+++ Music/MusicUtilities/helpers/administrator.py
@@ -1,25 +1,27 @@
-from Music import SUDOERS, app
+from functools import wraps
+
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 from pyrogram.types import Message
+from Music import app, SUDOERS
+from Music.Plugins.admins import member_permissions
 
 
 async def authorised(message):
-    message.chat.id
+    chatID = message.chat.id
     return 0
 
 
 async def unauthorised(message: Message):
     chatID = message.chat.id
     text = (
-        "Anda tidak memiliki izin yang diperlukan untuk melakukan tindakan ini."
-        + f"\nMEMBUTUHKAN ADMIN FULL "
+        "You don't have the required permission to perform this action."
+        + f"\n__REQUIRES ADMIN WITH MANAGE VC RIGHTS__"
     )
     try:
         await message.reply_text(text)
     except ChatWriteForbidden:
         await app.leave_chat(chatID)
     return 1
-
 
 async def adminsOnly(permission, message):
     chatID = message.chat.id
@@ -31,4 +33,4 @@ async def adminsOnly(permission, message):
     permissions = await member_permissions(chatID, userID)
     if userID not in SUDOERS and permission not in permissions:
         return await unauthorised(message)
-    return await authorised(message)
+    return await authorised( message)
