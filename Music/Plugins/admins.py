c414c0dc1a7143f9134786524087a5b4245c1277
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2021-11-22 16:15:35 +0700

    Update admins.py

--- Music/Plugins/admins.py
+++ Music/Plugins/admins.py
@@ -57,10 +57,7 @@ from Music.MusicUtilities.helpers.thumbnails import gen_thumb
 from Music.MusicUtilities.helpers.chattitle import CHAT_TITLE
 from Music.MusicUtilities.helpers.ytdl import ytdl_opts 
 from Music.MusicUtilities.helpers.inline import (
-    play_keyboard,
-    search_markup,
     play_markup,
-    playlist_markup,
     audio_markup,
 )
 from Music.MusicUtilities.tgcallsrun import (convert, download)
