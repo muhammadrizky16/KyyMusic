2d8fab9613e737a539c861d0474459bdeb5a708a
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2021-12-01 18:42:41 +0700

    Update inline.py

--- Music/MusicUtilities/helpers/inline.py
+++ Music/MusicUtilities/helpers/inline.py
@@ -14,7 +14,7 @@ def play_markup(videoid, user_id):
         ],
         [      
                 InlineKeyboardButton(text="ᴍᴇɴᴜ ⚙", callback_data=f"other {videoid}|{user_id}")
-            ],
+        ],
     ]
     return buttons
 
