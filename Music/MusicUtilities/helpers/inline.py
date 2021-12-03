eaf35cf0372e6edadcc740bbc3e88167f5556339
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2021-12-04 01:00:58 +0700

    Update inline.py

--- Music/MusicUtilities/helpers/inline.py
+++ Music/MusicUtilities/helpers/inline.py
@@ -223,7 +223,9 @@ play_list_keyboard = InlineKeyboardMarkup(
 def playlist_markup(user_name, user_id):
     buttons= [
             [
-                InlineKeyboardButton(text=f"Groups", callback_data=f'play_playlist {user_id}|group'),
+                InlineKeyboardButton(text=f"ɢʀᴏᴜᴘs", callback_data=f'play_playlist {user_id}|group'),
+            ],
+            [
                 InlineKeyboardButton(text=f"{user_name[:8]}", callback_data=f'play_playlist {user_id}|personal'),
             ],
             [
