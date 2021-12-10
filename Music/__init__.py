758bec88be668e3065ba6b780505f9236f963db2
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2021-12-11 02:27:07 +0700

    Update __init__.py

--- Music/__init__.py
+++ Music/__init__.py
@@ -2,7 +2,7 @@ print("[INFO]: INITIALIZING")
 
 from pyrogram import Client
 import asyncio
-from Music.config import API_ID, API_HASH, BOT_TOKEN, MONGO_DB_URI, SUDO_USERS, UPDATES_CHANNEL, GROUP SUPPORT,
+from Music.config import API_ID, API_HASH, BOT_TOKEN, MONGO_DB_URI, SUDO_USERS, UPDATES_CHANNEL, GROUP SUPPORT
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 import time
 import uvloop
