8a4afca794c2a68cbff9eae217105bd925a516e3
Noissy Robot <95743557+RidwanMusicProject@users.noreply.github.com>
2021-12-13 02:08:23 +0700

    Update ping.py

--- Music/Plugins/ping.py
+++ Music/Plugins/ping.py
@@ -21,7 +21,7 @@ Disk: {disk}%
     return stats
 
 
-@app.on_message(filters.command("ping"))
+@app.on_message(filters.command("mping"))
 async def ping(_, message):
     uptime = await bot_sys_stats()
     start = datetime.now()
