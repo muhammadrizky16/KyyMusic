329a5b951b08763d8552b16737906a3e30b4664f
Noissy Robot <95743557+RidwanMusicProject@users.noreply.github.com>
2021-12-14 10:47:09 +0700

    Update play.py

--- Music/Plugins/play.py
+++ Music/Plugins/play.py
@@ -433,7 +433,7 @@ Unban terlebih dahulu untuk menggunakan
         thumb ="cache/IMG_20211105_143948_192.jpg"
         buttons = search_markup(ID1, ID2, ID3, ID4, ID5, duration1, duration2, duration3, duration4, duration5, user_id, query)
         await mystic.edit( 
-            f"**✨ Silahkan pilih lagu yang ingin anda putar**\n\n¹ <b>{title1}</b>\n  ┗ 💡 <u>__[HALBERT](https://t.me/rdwan_13)\n\n² <b>{title2}</b>\n  ┗ 💡 <u>__[More Information](https://t.me/{BOT_USERNAME}?start=info_{ID2})__</u>\n\n³ <b>{title3}</b>\n  ┗ 💡 <u>__[More Information](https://t.me/{BOT_USERNAME}?start=info_{ID3})__</u>\n\n⁴ <b>{title4}</b>\n  ┗ 💡 <u>__[More Information](https://t.me/{BOT_USERNAME}?start=info_{ID4})__</u>\n\n⁵ <b>{title5}</b>\n  ┗ 💡 <u>__[More Information](https://t.me/{BOT_USERNAME}?start=info_{ID5})__</u>",    
+            f"**✨ Silahkan pilih lagu yang ingin anda putar**\n\n¹ <b>{title1}</b>\n  ┗ 💡 <u>__[HALBERT](https://t.me/rdwan_13)\n\n² <b>{title2}</b>\n  ┗ 💡 <u>__[HALBERT](https://t.me/rdwan_13)\n\n³ <b>{title3}</b>\n  ┗ 💡 <u>__[HALBERT](https://t.me/rdwan_13)\n\n⁴ <b>{title4}</b>\n  ┗ 💡 <u>__[HALBERT](https://t.me/rdwan_13)?\n\n⁵ <b>{title5}</b>\n  ┗ 💡 <u>__[SUPPORT](https://t.me/unclesamaja1)?start=info_{ID5})__</u>",    
             reply_markup=InlineKeyboardMarkup(buttons),
             disable_web_page_preview=True
         )  
