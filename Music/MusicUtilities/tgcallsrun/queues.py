47320b16e13e782854c7766d985798b5c16c0be9
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2021-12-16 15:54:59 +0700

    Update queues.py

--- Music/MusicUtilities/tgcallsrun/queues.py
+++ Music/MusicUtilities/tgcallsrun/queues.py
@@ -2,6 +2,8 @@ from asyncio import Queue as _Queue
 from asyncio import QueueEmpty as Empty
 from typing import Dict
 
+QUEUE = {}
+
 
 class Queue(_Queue):
     _queue: list = []
@@ -9,7 +11,9 @@ class Queue(_Queue):
     def clear(self):
         self._queue.clear()
 
+
 queues: Dict[int, Queue] = {}
+
 
 async def put(chat_id: int, **kwargs) -> int:
     if chat_id not in queues:
@@ -48,3 +52,37 @@ def clear(chat_id: int):
         else:
             queues[chat_id].clear()
     raise Empty
+
+
+def add_to_queue(chat_id, songname, link, ref, type, quality):
+    if chat_id in QUEUE:
+        chat_queue = QUEUE[chat_id]
+        chat_queue.append([songname, link, ref, type, quality])
+        return int(len(chat_queue) - 1)
+    else:
+        QUEUE[chat_id] = [[songname, link, ref, type, quality]]
+
+
+def get_queue(chat_id):
+    if chat_id in QUEUE:
+        chat_queue = QUEUE[chat_id]
+        return chat_queue
+    else:
+        return 0
+
+
+def pop_an_item(chat_id):
+    if chat_id in QUEUE:
+        chat_queue = QUEUE[chat_id]
+        chat_queue.pop(0)
+        return 1
+    else:
+        return 0
+
+
+def clear_queue(chat_id):
+    if chat_id in QUEUE:
+        QUEUE.pop(chat_id)
+        return 1
+    else:
+        return 0
