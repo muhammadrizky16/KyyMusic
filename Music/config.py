bfca1a623edf88f2e9d655fb0536416bc6474e6f
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2021-12-16 15:37:52 +0700

    Update config.py

--- Music/config.py
+++ Music/config.py
@@ -17,3 +17,9 @@ ASS_ID = int(getenv("ASS_ID", '2130437611'))
 OWNER_ID = list(map(int, getenv('OWNER_ID', '').split()))
 GROUP = getenv("GROUP", None)
 CHANNEL = getenv("CHANNEL", None)
+ASS_ACC = Client(
+    SESSION_NAME2,
+    API_ID,
+    API_HASH,
+)
+call_py = PyTgCalls(ASS_ACC)
