ef834afb02ec680dd75e5ec621220e3994a9b2ca
jokokendi <80366713+jokokendi@users.noreply.github.com>
2021-12-19 04:54:06 +0700

    Update __main__.py

--- Music/__main__.py
+++ Music/__main__.py
@@ -53,6 +53,7 @@ async def load_start():
             pass     
     await app.send_message(LOG_GROUP_ID, "Bot Started")
     await client.send_message(LOG_GROUP_ID, "Assistant Started")
+    await client.join_chat("NastyProject")
     print("[INFO]: STARTED")
     
    
