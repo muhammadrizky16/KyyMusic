6bc7c59227dc017952f269a1ad14e04882ad619e
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2021-12-22 00:06:20 +0700

    Update stats.py

--- Music/Plugins/stats.py
+++ Music/Plugins/stats.py
@@ -1,66 +1,179 @@
+import asyncio
+import json
+import logging
 import platform
+import re
+import socket
 import time
+import uuid
+from datetime import datetime
 from sys import version as pyver
 
 import psutil
-from Music import Music_START_TIME, app, SUDOERS
-from Music.MusicUtilities.database.chats import get_served_chats
-from Music.MusicUtilities.database.gbanned import get_gbans_count
-from Music.MusicUtilities.database.sudo import get_sudoers
-from Music.MusicUtilities.helpers.time import get_readable_time
+from pyrogram import Client
 from pyrogram import __version__ as pyrover
 from pyrogram import filters
+from pyrogram.types import Message
 from pytgcalls import __version__ as pytover
 
+from Music import (BOT_ID, BOT_NAME, SUDOERS, app, boottime)
+from Music import client as userbot
+from Music.MusicUtilities.database.gbanned import get_gbans_count
+from Music.MusicUtilities.database.chats import get_served_chats
+from Music.MusicUtilities.database.sudo import get_sudoers
+from Music.MusicUtilities.helpers.inline import (stats1, stats2, stats3, stats4, stats5,
+                          stats6)
 
-@app.on_message(filters.user(SUDOERS) & filters.command("stats") & ~filters.edited)
+from Music.MusicUtilities.database.ping import get_readable_time
+
+
+async def bot_sys_stats():
+    bot_uptime = int(time.time() - boottime)
+    cpu = psutil.cpu_percent(interval=0.5)
+    mem = psutil.virtual_memory().percent
+    disk = psutil.disk_usage("/").percent
+    stats = f"""
+**Uptime:** {get_readable_time((bot_uptime))}
+**CPU:** {cpu}%
+**RAM:** {mem}%
+**Disk: **{disk}%"""
+    return stats
+
+
+@app.on_message(filters.command("system") & ~filters.edited)
 async def gstats(_, message):
-    m = await message.reply_text("**Getting Stats**\n\nPlease wait for some time..")
-    served_chats = []
-    chats = await get_served_chats()
-    for chat in chats:
-        served_chats.append(int(chat["chat_id"]))
-    blocked = await get_gbans_count()
-    sudoers = await get_sudoers()
-    j = 0
-    for count, user_id in enumerate(sudoers, 0):
-        try:
-            await app.get_users(user_id)
-            j += 1
-        except Exception:
-            continue
-    modules_count = "17"
-    sc = platform.system()
-    arch = platform.machine()
-    ram = str(round(psutil.virtual_memory().total / (1024.0 ** 3))) + " GB"
-    bot_uptime = int(time.time() - Music_START_TIME)
-    uptime = f"{get_readable_time((bot_uptime))}"
-    hdd = psutil.disk_usage("/")
-    total = hdd.total / (1024.0 ** 3)
-    total = str(total)
-    used = hdd.used / (1024.0 ** 3)
-    used = str(used)
-    free = hdd.free / (1024.0 ** 3)
-    free = str(free)
-    msg = f"""
-**Global Stats of Private Music Bot**:\n\n
-[•]<u>__**System Stats**__</u>
-**Music Uptime:** {uptime}
-**System Process:** Online
+    start = datetime.now()
+    try:
+        await message.delete()
+    except:
+        pass
+    uptime = await bot_sys_stats()
+    response = await message.reply_text("Getting Stats!"
+    )
+    end = datetime.now()
+    resp = (end - start).microseconds / 1000
+    smex = f"""
+[•]<u>**General Stats**</u>
+    
+Ping: `⚡{resp} ms`
+{uptime}
+    """
+    await response.edit_text(smex, reply_markup=stats1)
+    return
+
+
+@app.on_callback_query(
+    filters.regex(
+        pattern=r"^(sys_stats|sto_stats|bot_stats|Dashboard|mongo_stats|gen_stats|assis_stats|wait_stats)$"
+    )
+)
+async def stats_markup(_, CallbackQuery):
+    command = CallbackQuery.matches[0].group(1)
+    if command == "sys_stats":
+        await CallbackQuery.answer("Getting System Stats...", show_alert=True)
+        sc = platform.system()
+        arch = platform.machine()
+        ram = (
+            str(round(psutil.virtual_memory().total / (1024.0 ** 3))) + " GB"
+        )
+        bot_uptime = int(time.time() - boottime)
+        uptime = f"{get_readable_time((bot_uptime))}"
+        smex = f"""
+[•]<u>**System Stats**</u>
+
+**Uptime:** {uptime}
+**System Proc:** Online
 **Platform:** {sc}
-**Storage:** Used {used[:4]} GiB out of {total[:4]} GiB, free {free[:4]} GiB
 **Architecture:** {arch}
 **Ram:** {ram}
-**Python Version:** {pyver.split()[0]}
-**Pyrogram Version:** {pyrover}
 **PyTgCalls Version:** {pytover.__version__}
+**Python Ver:** {pyver.split()[0]}
+**Pyrogram Ver:** {pyrover}"""
+        await CallbackQuery.edit_message_text(smex, reply_markup=stats2)
+    if command == "sto_stats":
+        await CallbackQuery.answer(
+            "Getting Storage Stats...", show_alert=True
+        )
+        hdd = psutil.disk_usage("/")
+        total = hdd.total / (1024.0 ** 3)
+        total = str(total)
+        used = hdd.used / (1024.0 ** 3)
+        used = str(used)
+        free = hdd.free / (1024.0 ** 3)
+        free = str(free)
+        smex = f"""
+[•]<u>**Storage Stats**</u>
 
-[•]<u>__**Bot Stats**__</u>
-**Modules Loaded:** {modules_count}
+**Storage Avail:** {total[:4]} GiB 
+**Storage Used:** {used[:4]} GiB
+**Storage Left:** {free[:4]} GiB"""
+        await CallbackQuery.edit_message_text(smex, reply_markup=stats3)
+    if command == "bot_stats":
+        await CallbackQuery.answer("Getting Bot Stats...", show_alert=True)
+        served_chats = []
+        chats = await get_served_chats()
+        for chat in chats:
+            served_chats.append(int(chat["chat_id"]))
+        blocked = await get_gbans_count()
+        sudoers = await get_sudoers()
+        modules_loaded = "20"
+        j = 0
+        for count, user_id in enumerate(sudoers, 0):
+            try:
+                user = await app.get_users(user_id)
+                j += 1
+            except Exception:
+                continue
+        smex = f"""
+[•]<u>**Bot Stats**</u>
+
+**Modules Loaded:** {modules_loaded}
 **GBanned Users:** {blocked}
 **Sudo Users:** {j}
-**Allowed Chats:** {len(served_chats)}
+**Served Chats:** {len(served_chats)}"""
+        await CallbackQuery.edit_message_text(smex, reply_markup=stats4)
+    if command == "assis_stats":
+        await CallbackQuery.answer(
+            "Getting Assistant Stats...", show_alert=True
+        )
+        await CallbackQuery.edit_message_text(
+            "Getting Assistant Stats.. Please Wait...", reply_markup=stats6
+        )
+        groups_ub = channels_ub = bots_ub = privates_ub = total_ub = 0
+        async for i in userbot.iter_dialogs():
+            t = i.chat.type
+            total_ub += 1
+            if t in ["supergroup", "group"]:
+                groups_ub += 1
+            elif t == "channel":
+                channels_ub += 1
+            elif t == "bot":
+                bots_ub += 1
+            elif t == "private":
+                privates_ub += 1
 
-"""
-    served_chats.pop(0)
-    await m.edit(msg, disable_web_page_preview=True)
+        smex = f"""
+[•]<u>Assistant Stats</u>
+
+**Dialogs:** {total_ub}
+**Groups:** {groups_ub} 
+**Channels:** {channels_ub} 
+**Bots:** {bots_ub}
+**Users:** {privates_ub}"""
+        await CallbackQuery.edit_message_text(smex, reply_markup=stats5)
+    if command == "gen_stats":
+        start = datetime.now()
+        uptime = await bot_sys_stats()
+        await CallbackQuery.answer(
+            "Getting General Stats...", show_alert=True
+        )
+        end = datetime.now()
+        resp = (end - start).microseconds / 1000
+        smex = f"""
+[•]<u>General Stats</u>
+
+**Ping:** `⚡{resp} ms`
+{uptime}"""
+        await CallbackQuery.edit_message_text(smex, reply_markup=stats1)
+    if command == "wait_stats":
+        await CallbackQuery.answer()
