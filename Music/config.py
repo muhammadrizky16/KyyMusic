ec65f5463bb9d43f81e74c4ec9c50f8a971b09ac
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2021-12-27 23:42:46 +0700

    Update config.py

--- Music/config.py
+++ Music/config.py
@@ -4,6 +4,7 @@ from os import getenv
 from dotenv import load_dotenv
 
 load_dotenv()
+get_queue = {}
 SESSION_NAME = getenv('SESSION_NAME', 'session')
 BOT_TOKEN = getenv('BOT_TOKEN')
 API_ID = int(getenv('API_ID', "10892147"))
