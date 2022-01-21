d53d7531e3488349ea0d22d89d3bc39006fe9e27
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-01-21 21:49:34 +0700

    Update paste.py

--- Music/MusicUtilities/helpers/paste.py
+++ Music/MusicUtilities/helpers/paste.py
@@ -1,6 +1,7 @@
 import socket
 from asyncio import get_running_loop
 from functools import partial
+from Music import aiohttpsession as session
 
 
 def _netcat(host, port, content):
@@ -22,3 +23,16 @@ async def paste(content):
         None, partial(_netcat, "ezup.dev", 9999, content)
     )
     return link
+
+async def isPreviewUp(preview: str) -> bool:
+    for _ in range(7):
+        try:
+            async with session.head(preview, timeout=2) as resp:
+                status, size = resp.status, resp.content_length
+        except asyncio.exceptions.TimeoutError:
+            return False
+        if status == 404 or (status == 200 and size == 0):
+            await asyncio.sleep(0.4)
+        else:
+            return True if status == 200 else False
+    return False
