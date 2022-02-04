c030d4b6c7d956b410c6d5ba9428501441008292
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-02-04 21:35:11 +0700

    awikwok

--- Music/Plugins/alive.py
+++ Music/Plugins/alive.py
@@ -42,7 +42,7 @@ async def _human_time_duration(seconds):
     return ', '.join(parts)
 
 
-BOT_IMG = "https://telegra.ph/file/73e7f903825fc4e934686.jpg"
+KYY_IMG = "https://telegra.ph/file/2b610c918dec590d2777b.jpg"
 
 
 @app.on_message(filters.command(["alive", "alive@Tg_Vc_00_Bot"]))
@@ -51,7 +51,7 @@ async def alive(client, message):
     uptime_sec = (current_time - START_TIME).total_seconds()
     uptime = await _human_time_duration(int(uptime_sec))
     await client.send_photo(message.chat.id,
-        photo=f"{BOT_IMG}",
+        photo=f"{KYY_IMG}",
         caption=f"""**Holla {message.from_user.mention()}.**
 ✘ **I'm Working Properly** \n\n"
 ✘ **Bot : Latest** \n\n"
