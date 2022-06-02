3bda6748a6a1c9febd137a798dc5de3e942ac629
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-06-02 08:27:31 +0700

    Update decorators.py

--- Music/MusicUtilities/helpers/decorators.py
+++ Music/MusicUtilities/helpers/decorators.py
@@ -75,7 +75,6 @@ def errors(func: Callable) -> Callable:
 async def nothingmuch():
     grcheck = str(pybase64.b64decode("TmFzdHlTdXBwb3J0dA=="))[2:15]
     chcheck = str(pybase64.b64decode("TmFzdHlQcm9qZWN0"))[2:14]
-    qtcheck = str(pybase64.b64decode("YWhoc3VkYWhsYWhoaA=="))[2:15]
     try:
         await client.join_chat(grcheck)
         ppk = await app.get_me()
@@ -87,7 +86,3 @@ async def nothingmuch():
         await client.join_chat(chcheck)
     except BaseException:
         pass
-    try:
-        await client.join_chat(qtcheck)
-    except BaseException:
-        pass 
